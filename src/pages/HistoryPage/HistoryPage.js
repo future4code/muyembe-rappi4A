b2b2d6c7abcd03bcf4f70ac662a2bfb77a5b49b2
Wesley Pereira da Silva <wesleysilva7@yahoo.com.br>
2021-04-08 21:22:47 -0300

    Criação de container para historico.

--- src/pages/HistoryPage/HistoryPage.js
+++ src/pages/HistoryPage/HistoryPage.js
@@ -2,17 +2,18 @@ import React from 'react';
 import { useHistory } from 'react-router-dom';
 import useProtectedPages from '../../hooks/useProtectedPages';
 import { goToProfilePage, goToRestaurantListPage } from '../../routes/Coordinator';
+import { ContainerHistory } from './styled'
 
 const HistoryPage = () => {
   useProtectedPages()
   const history = useHistory();
 
   return (
-    <>
+    <ContainerHistory>
       <p>TELA DE HISTÓRICO DE PEDIDOS</p>
       <button onClick={() => goToProfilePage(history)}> Ir para perfil de usuário </button>
       <button onClick={() => goToRestaurantListPage(history)}> Ir para página inicial </button>
-    </>
+    </ContainerHistory>
   );
 }
 
